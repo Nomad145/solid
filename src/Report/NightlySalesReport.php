d89376f6ef8a24b48a073f5577a24e1adfe795af
Michael Phillips <michaeljoelphillips@gmail.com>
2018-06-12 00:43:02 -0500

    Add NightlySalesReport.

---
+++ src/Report/NightlySalesReport.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Report;
+
+use InvalidArgumentException;
+
+/**
+ * @author Michael Phillips <michael.phillips@realpage.com>
+ */
+class NightlySalesReport
+{
+    private const NAME = 'Nightly Sales Report';
+
+    private $recipients = [];
+
+    public function __construct(array $recipients)
+    {
+        if (!$this->recipientsAreValid($recipients)) {
+            throw new InvalidArgumentException('Invalid email address given');
+        }
+
+        $this->recipients = $recipients;
+    }
+
+    private function recipientsAreValid(array $recipients): bool
+    {
+        foreach ($recipients as $recipient) {
+            if (!filter_var($recipient, FILTER_VALIDATE_EMAIL)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getRecipients(): array
+    {
+        return $this->recipients;
+    }
+}
---
+++ tests/Report/NightlySalesReportTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Report;
+
+use PHPUnit\Framework\TestCase;
+use InvalidArgumentException;
+use App\Report\NightlySalesReport;
+
+/**
+ * @author Michael Phillips <michael.phillips@realpage.com>
+ */
+class NightlySalesNightlySalesReportTest extends TestCase
+{
+    public function testNightlySalesReport()
+    {
+        $recipients = ['foo@gmail.com', 'bar@yahoo.com'];
+
+        $subject = new NightlySalesReport($recipients);
+
+        self::assertEquals($recipients, $subject->getRecipients());
+        self::assertEquals('Nightly Sales Report', $subject->getName());
+    }
+
+    public function testThrowsInvalidArgumentException()
+    {
+        $recipients = ['foo@invaliddomain'];
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $subject = new NightlySalesReport($recipients);
+    }
+}
